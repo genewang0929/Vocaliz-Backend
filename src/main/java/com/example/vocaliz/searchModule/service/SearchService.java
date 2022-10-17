9cef3cfe2ce7682fc4ab8f08c43c4ec0898d09e4
genewang <genewang7@gmail.com>
2022-10-17 13:04:56 +0800

    fix- searchReturn

--- src/main/java/com/example/vocaliz/searchModule/service/SearchService.java
+++ src/main/java/com/example/vocaliz/searchModule/service/SearchService.java
@@ -1,6 +1,9 @@
 package com.example.vocaliz.searchModule.service;
 
+import com.example.vocaliz.categoryModule.entity.*;
+import com.example.vocaliz.categoryModule.service.*;
 import com.example.vocaliz.repository.*;
+import com.example.vocaliz.searchModule.entity.*;
 import com.example.vocaliz.vocabularyModule.entity.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.data.domain.*;
@@ -13,6 +16,8 @@ import java.util.stream.*;
 public class SearchService {
     @Autowired
     private VocabularyRepository vocabularyRepository;
+    @Autowired
+    private CategoryService categoryService;
 
     public Pages searchVocabulary(String word, String email, int offset, int pageNumber) {
         if (word.equals(""))
@@ -23,10 +28,20 @@ public class SearchService {
                 .filter((Vocabulary v) -> v.getCreatorEmail().equals(email))
                 .collect(Collectors.toList());
 
+        List<SearchReturn> searchReturnList = new ArrayList<>();
+        for (Vocabulary vocabulary : vocabularyList) {
+            SearchReturn searchReturn = new SearchReturn();
+            searchReturn.setWord(vocabulary.getWord());
+            searchReturn.setDefinition(vocabulary.getDefinition());
+            Category category = categoryService.getACategory(vocabulary.getParentCategory());
+            searchReturn.setParentCategoryName(category.getCategoryName());
+            searchReturnList.add(searchReturn);
+        }
+
         Pageable paging = PageRequest.of(offset, pageNumber);
-        int start = Math.min((int)paging.getOffset(), vocabularyList.size());
-        int end = Math.min((start + paging.getPageSize()), vocabularyList.size());
-        Page<Vocabulary> page = new PageImpl<>(vocabularyList.subList(start, end), paging, vocabularyList.size());
+        int start = Math.min((int)paging.getOffset(), searchReturnList.size());
+        int end = Math.min((start + paging.getPageSize()), searchReturnList.size());
+        Page<SearchReturn> page = new PageImpl<>(searchReturnList.subList(start, end), paging, searchReturnList.size());
         return new Pages(page.getContent(), page.getTotalPages());
     }
 }
